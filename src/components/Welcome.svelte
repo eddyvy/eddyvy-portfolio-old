4dfd63672e34c4535158acf7b604294200782981
eddyvy <edwardyue93@gmail.com>
2021-03-15 19:38:41 +0100

    First commit

---
+++ src/components/Welcome.svelte
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<div class="Welcome">
+    <h1>Welcome to my portfolio <br> I am Eduard Valls <br> Nice to meet you</h1>
+</div>
+
+<style>
+    .Welcome {
+        text-align: center;
+    }
+</style>
---
+++ src/containers/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import Welcome from '../components/Welcome.svelte'
+</script>
+
+<div class="App">
+  <Welcome />
+</div>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
+  :global(main) {
+    color: rgb(15, 131, 240);
+    font-family: 'Open Sans', sans-serif;
+  }
+  
+  .App {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+  }
+</style>
